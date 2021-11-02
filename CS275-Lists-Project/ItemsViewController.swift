38592a0df85c46b4008b225954c1a10d0349a00d
spitonya4 <spitonya@uvm.edu>
2021-11-02 11:12:53 -0400

    Got alert controller working for when deleting from table view

--- CS275-Lists-Project/ItemsViewController.swift
+++ CS275-Lists-Project/ItemsViewController.swift
@@ -68,29 +68,24 @@ class ItemsViewController: UITableViewController {
         if editingStyle == .delete {
             let item = itemStore.allItems[indexPath.row]
             // Create an alert controller
-//            let alertController = UIAlertController(title: nil,
-//                                                    message: "Are you sure you want to delete \(item.teamName)",
-//                                                    preferredStyle: .alert)
-//
-//            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-//            alertController.addAction(cancelAction)
-//
-//            let deleteAction = UIAlertAction(title: "Yes", style: .destructive) {_ in
-//                // Remove the item from the store
-//                self.itemStore.removeItem(item)
-//
-//                // Also remove that row from the table view with an animation
-//                self.tableView.deleteRows(at: [indexPath], with: .automatic)
-//            }
-//            alertController.addAction(cancelAction)
-//            alertController.addAction(deleteAction)
-//
-//            present(alertController, animated: true, completion: nil)
-            // Remove the item from the store
-            itemStore.removeItem(item)
+            let alertController = UIAlertController(title: nil,
+                                                    message: "Are you sure you want to delete \(item.teamName)",
+                                                    preferredStyle: .alert)
 
-            // Also remove that row from the table view with an animation
-            tableView.deleteRows(at: [indexPath], with: .automatic)
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+
+            let deleteAction = UIAlertAction(title: "Yes", style: .destructive) {_ in
+                // Remove the item from the store
+                self.itemStore.removeItem(item)
+
+                // Also remove that row from the table view with an animation
+                self.tableView.deleteRows(at: [indexPath], with: .automatic)
+            }
+            
+            alertController.addAction(cancelAction)
+            alertController.addAction(deleteAction)
+
+            present(alertController, animated: true, completion: nil)
         }
     }
     
